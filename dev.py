6046a161533360bdcec1f3e4fa9972aea15ee74f
Alassane TINE <94652168+aklassanetine94@users.noreply.github.com>
2026-06-17 16:53:11 +0200

    Update dev.py

--- dev.py
+++ dev.py
@@ -7,7 +7,7 @@ Created on Mon Oct 10 10:15:29 2022
 #####################################
 import streamlit as st  # pip install streamlit
 import pandas as pd  # pip install pandas
-import plotly.express as px
+import plotly.express as px  # pip install plotly-express
 import base64  # Standard Python Module
 from io import StringIO, BytesIO  # Standard Python Module
 
@@ -91,51 +91,21 @@ with right_column:
 
 
 
-import plotly.express as px
+#import plotly.express as px
 st.markdown("""---""")
 
 # SALES BY PRODUCT LINE [BAR CHART]
 sales_by_product_line = (
     df.groupby(by=["RestingECG"]).sum()[["Age"]].sort_values(by="Age")
 )
-fig_product_sales = px.bar(
-    sales_by_product_line,
-    x="Age",
-    y=sales_by_product_line.index,
-    #orientation="h",
-    title="<b>Sales by Product Line</b>",
-    color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
-    template="plotly_white",
-)
-fig_product_sales.update_layout(
-    plot_bgcolor="rgba(0,0,0,0)",
-    xaxis=(dict(showgrid=False))
-)
 
 
-
-sales_by_hour = df.groupby(by=["ChestPainType"]).sum()[["Age"]]
-fig_hourly_sales = px.bar(
-    sales_by_hour,
-    x=sales_by_hour.index,
-    y="Age",
-    #orientation="h",
-    title="<b>Sales by hour</b>",
-    color_discrete_sequence=["#0083B8"] * len(sales_by_hour),
-    template="plotly_white",
-)
-fig_hourly_sales.update_layout(
-    xaxis=dict(tickmode="linear"),
-    plot_bgcolor="rgba(0,0,0,0)",
-    yaxis=(dict(showgrid=False)),
-)
 
 
 
 
 left_column, right_column = st.columns(2)
-left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
-right_column.plotly_chart(fig_product_sales, use_container_width=True)
+
 
 
 st.write(df)
